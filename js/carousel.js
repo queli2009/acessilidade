582d91504599a697e7996603101d653c3cb9513d
queli2009 <mariano.raqueli@escola.pr.gov.br>
2025-11-07 08:39:46 -0300

    carosel.js

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+var btns = document.querySelectorAll('.listaDeArtigo-slider-item');
+
+//Percorrer todos os botoes controladores
+btns.forEach(function(btn){
+   btn.addEventListener('click', function()) {
+
+
+    //Remover class 'ativo' dos outros botoes
+    btns.forEach(function(btnRomoveClass) {
+       btnRomoveClass.classList.remove('listaDeArtigo-slider-item-ativo')
+    })
+
+    this.classList.add('listaDeArtigo-slider-item--ativo')
+   }
+}
